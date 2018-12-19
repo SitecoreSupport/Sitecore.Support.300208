f2c0e1f23d022a939d579b7f65990cdb233045af
DK\IEKA <ieka@sitecore.net>
2018-12-19 11:49:12 +0200

    Fix "NullReferenceException" when rebuilding analytics index.

---
+++ src/Sitecore.Support.300208/XA/Foundation/Search/ComputedFields/Inheritance.cs
@@ -0,0 +1,26 @@
+﻿using Sitecore.Buckets.Util;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.ComputedFields;
+using Sitecore.Data.Items;
+using System.Linq;
+
+namespace Sitecore.Support.XA.Foundation.Search.ComputedFields
+{
+  public class Inheritance : AbstractComputedIndexField
+  {
+    public override object ComputeFieldValue(IIndexable indexable)
+    {
+      Item item = null;
+      if (indexable is SitecoreIndexableItem)
+      {
+        item = ((Item)(indexable as SitecoreIndexableItem));
+      }
+      else
+      {
+        return null;
+      }
+      return from template in item.Template.BaseTemplates
+             select IdHelper.NormalizeGuid(template.ID.ToString(), true);
+    }
+  }
+}
